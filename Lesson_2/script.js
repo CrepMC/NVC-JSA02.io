0a19e0133664bb06ec26243c98ab39aed9fe5584
nhan <nhan069069@stu.vinschool.edu.vn>
2024-04-07 08:38:24 +0700

    hah

---
+++ Lesson_2/script.js
@@ -0,0 +1,32 @@
+// for (let index = -1000000; index < 100; index++) {
+//     console.log(`Age: ${index}`);
+// }
+
+// let i = 0;
+// while (i <= 40) {
+//     console.log(i);
+//     i++;
+// }
+
+// let age;
+
+// do {
+//     age = prompt("Nhập tuổi 6-90");
+// } while (age < 6 || age > 90)
+
+
+do {
+    let n = prompt("NHẬP n dương =");
+} while (n <= 0)
+
+sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum = sum + i;
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     if (i % 2 == 0) {
+//         console.log('sum')
+//     }
+
+// }
